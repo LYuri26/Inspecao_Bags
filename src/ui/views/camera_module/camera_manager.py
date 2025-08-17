d586ba12d6097ec5d15bbe94d50655726e9822b0
Lenon <lenonyurisilva26@gmail.com>
2025-08-17 18:04:31 -0300

    17/08/25

--- src/ui/views/camera_module/camera_manager.py
+++ src/ui/views/camera_module/camera_manager.py
@@ -188,7 +188,7 @@ class CameraManager(QObject):
 
         # Configuração das URLs RTSP para todas as 9 câmeras
         self.camera_urls: List[Optional[str]] = [
-            "rtsp://admin:Evento0128@192.168.1.101:559/Streaming/Channels/101",  # Cam 0 "rtsp://admin:Solutions10@@192.168.0.241:554/Streaming/Channels/101",  # Cam 0
+            "rtsp://admin:Solutions10@@192.168.0.241:554/Streaming/Channels/101",  # Cam 0 "rtsp://admin:Evento0128@192.168.1.101:559/Streaming/Channels/101",  # Cam 0
             None,  # "rtsp://admin:Solutions10@@192.168.0.242:554/Streaming/Channels/101",  # Cam 1
             None,  # "rtsp://admin:Solutions10@@192.168.0.243:554/Streaming/Channels/101",  # Cam 2
             None,  # "rtsp://admin:Solutions10@@192.168.0.244:554/Streaming/Channels/101",  # Cam 3
